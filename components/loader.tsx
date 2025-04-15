afafe3460a942e65c473b4080fc39ce1bb7dd8b1
a96363877 <a96363877@gmail.com>
2025-04-15 17:50:00 +0300

    sda

---
+++ components/loader.tsx
@@ -0,0 +1,17 @@
+
+import * as React from 'react'
+export default function Loader(){
+
+  return (
+      <div className="site-loader" >
+          <span></span>
+          <p>
+              <img src="/loader.png" alt="kicLoader" title="Kuwait Insurance Company" />
+          </p>
+      </div>
+  )
+}
+
+
+
+
