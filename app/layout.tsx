61b859dba1ec60dc1c2d7389d1f86182d4e99113
a96363877 <a96363877@gmail.com>
2025-04-16 12:54:41 +0300

    ss

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  </head>
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Header />
